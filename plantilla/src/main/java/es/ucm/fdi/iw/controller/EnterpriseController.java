89b4414c87d7b8b378b1b005b9ad991c8124a69f
alexboni97 <alexboni@ucm.es>
2025-02-10 19:50:54 +0100

    request paramn added

--- plantilla/src/main/java/es/ucm/fdi/iw/controller/EnterpriseController.java
+++ plantilla/src/main/java/es/ucm/fdi/iw/controller/EnterpriseController.java
@@ -6,10 +6,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import jakarta.servlet.http.HttpSession;
 
 @Controller
+@RequestMapping("enterprise")
 public class EnterpriseController {
 
     @ModelAttribute
