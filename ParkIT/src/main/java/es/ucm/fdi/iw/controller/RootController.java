225b1fb86841ae864114e03e9597d015ce02754e
juanpf04 <juanpf04@ucm.es>
2025-03-20 19:23:21 +0100

    poblar bbdd con persist auto

--- ParkIT/src/main/java/es/ucm/fdi/iw/controller/RootController.java
+++ ParkIT/src/main/java/es/ucm/fdi/iw/controller/RootController.java
@@ -3,23 +3,16 @@ package es.ucm.fdi.iw.controller;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import jakarta.persistence.EntityManager;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
-import jakarta.transaction.Transactional;
 
 import es.ucm.fdi.iw.model.*;
-
-import java.time.LocalTime;
-import java.time.LocalDate;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,9 +24,6 @@ import java.util.List;
 public class RootController {
 
     private static final Logger log = LogManager.getLogger(RootController.class);
-
-    @Autowired
- 	private PasswordEncoder passwordEncoder;
 
     @Autowired
 	private EntityManager entityManager;
@@ -65,265 +55,6 @@ public class RootController {
         for (Parking p : parkings) {
             System.out.println("Parking: " + p.getName());
         }
-
-        return "index";
-    }
-
-    @PostMapping("/")
-    @Transactional
-    public String poblarUsuarios() {
-        
-        Admin a = new Admin();
-        //a.setId(1);
-        a.setEnabled(true);
-        a.setRole(User.Role.ADMIN);
-        a.setUsername("a");
-        a.setPassword(passwordEncoder.encode("aa"));
-        a.setWallet(10000);
-        a.setEmail("1234@park-it.com");
-        a.setTelephone(000000000);
-        a.setCodigo("1234");
-        entityManager.persist(a);
-
-        Parker u = new Parker();
-        //u.setId(2);
-        u.setEnabled(true);
-        u.setRole(User.Role.USER);
-        u.setUsername("b");
-        u.setPassword(passwordEncoder.encode("aa"));
-        u.setWallet(10000);
-        u.setEmail("pepe@gmail.com");
-        u.setTelephone(000000001);
-        u.setDNI("aaa");
-        u.setFirstName("Pepe");
-        u.setSecondName("Perez");
-        entityManager.persist(u);
-
-        Enterprise e = new Enterprise();
-        //e.setId(3);
-        e.setEnabled(true);
-        e.setRole(User.Role.ENTERPRISE);
-        e.setUsername("e");
-        e.setPassword(passwordEncoder.encode("aa"));
-        e.setWallet(0);
-        e.setEmail("e@empresa1.com");
-        e.setTelephone(000000002);
-        e.setName("Empresa1");
-        e.setCIF("ES1234");
-        entityManager.persist(e);
-
-        Parker u1 = new Parker();
-        //u1.setId(4);
-        u1.setEnabled(true);
-        u1.setRole(User.Role.USER);
-        u1.setUsername("paulal10");
-        u1.setPassword(passwordEncoder.encode("aa"));
-        u1.setWallet(517);
-        u1.setEmail("lamejor@ucm.es");
-        u1.setTelephone(000000002);
-        u1.setDNI("aab");
-        u1.setFirstName("Paula");
-        u1.setSecondName("López");
-        entityManager.persist(u1);
-        
-        Parker u2 = new Parker();
-        //u2.setId(5);
-        u2.setEnabled(true);
-        u2.setRole(User.Role.USER);
-        u2.setUsername("secsanc");
-        u2.setPassword(passwordEncoder.encode("aa"));
-        u2.setWallet(517);
-        u2.setEmail("lamejor@ucm.es");
-        u2.setTelephone(000000003);
-        u2.setDNI("aac");
-        u2.setFirstName("Sergio");
-        u2.setSecondName("Sanchez");
-        entityManager.persist(u2);
-
-        Parking parking1 = new Parking();
-        //parking1.setId(1);
-        parking1.setName("Parking 1");
-        parking1.setAddress("Calle de la piruleta");
-        parking1.setTelephone(123456789);
-        parking1.setEmail("parking1@gmail.com");
-        parking1.setEnterprise(e);
-        parking1.setOpeningTime(LocalTime.parse("08:00"));
-        parking1.setClosingTime(LocalTime.parse("20:00"));
-        parking1.setFeePerHour(1.5);
-        parking1.setEnabled(true);
-        parking1.setLatitude(40.416775);
-        parking1.setLongitude(-3.703790);
-        parking1.setWidth(20.0); // Example value
-        parking1.setHeight(10.0); // Example value
-        parking1.setEntryX(1.0); // Example value
-        parking1.setEntryY(1.0); // Example value
-        parking1.setExitX(2.0); // Example value
-        parking1.setExitY(2.0); // Example value
-        entityManager.persist(parking1);
-
-        Parking parking2 = new Parking();
-        //parking2.setId(2);
-        parking2.setName("Parking 2");
-        parking2.setAddress("Calle de la fresa");
-        parking2.setTelephone(123456789);
-        parking2.setEmail("parking2@gmail.com");
-        parking2.setEnterprise(e);
-        parking2.setOpeningTime(LocalTime.parse("08:00"));
-        parking2.setClosingTime(LocalTime.parse("20:00"));
-        parking2.setFeePerHour(1.5);
-        parking2.setEnabled(true);
-        parking2.setLatitude(42.416775);
-        parking2.setLongitude(-5.703790);
-        parking2.setWidth(20.0); // Example value
-        parking2.setHeight(10.0); // Example value
-        parking2.setEntryX(1.0); // Example value
-        parking2.setEntryY(1.0); // Example value
-        parking2.setExitX(2.0); // Example value
-        parking2.setExitY(2.0); // Example value
-        entityManager.persist(parking2);
-
-        Parking parking3 = new Parking();
-        //parking3.setId(3);
-        parking3.setName("Parking 3");
-        parking3.setAddress("Paseo de los Olmos");
-        parking3.setTelephone(123456789);
-        parking3.setEmail("parking3@gmail.com");
-        parking3.setEnterprise(e);
-        parking3.setOpeningTime(LocalTime.parse("08:00"));
-        parking3.setClosingTime(LocalTime.parse("20:00"));
-        parking3.setFeePerHour(1.5);
-        parking3.setEnabled(true);
-        parking3.setLatitude(40.404711);
-        parking3.setLongitude(-3.710862);
-        parking3.setWidth(20.0); // Example value
-        parking3.setHeight(10.0); // Example value
-        parking3.setEntryX(1.0); // Example value
-        parking3.setEntryY(1.0); // Example value
-        parking3.setExitX(2.0); // Example value
-        parking3.setExitY(2.0); // Example value
-        entityManager.persist(parking3);
-
-        for (int j=1; j <= 13; j++) { // por ahora las plazas estan en el parking 3
-            Spot spot = new Spot();
-            //spot.setId(j);
-            spot.setParking(parking3);
-            spot.setEnabled(true); // Example value
-            spot.setSize(Math.random() > 0.5 ? "M" : "L"); // Example value
-            spot.setHorizontal(true); // Example value
-            spot.setX(j * 2.5); // Example value
-            spot.setY(0.0); // Example value
-            entityManager.persist(spot);
-        }
-
-        Vehicle v1 = new Vehicle();
-        //v1.setId(1);
-        v1.setEnabled(true);
-        v1.setPlate("ABC1234");
-        v1.setBrand("Toyota");
-        v1.setModel("Corolla");
-        v1.setSize("M");
-        v1.setParker(u);
-        entityManager.persist(v1);
-
-        Vehicle v2 = new Vehicle();
-        //v2.setId(2);
-        v2.setEnabled(true);
-        v2.setPlate("XYZ5678");
-        v2.setBrand("Honda");
-        v2.setModel("Civic");
-        v2.setSize("L");
-        v2.setParker(u);
-        entityManager.persist(v2);
-
-        Vehicle v3 = new Vehicle();
-        //v3.setId(3);
-        v3.setEnabled(true);
-        v3.setPlate("DEF9877");
-        v3.setBrand("BMW");
-        v3.setModel("X5");
-        v3.setSize("XL");
-        v3.setParker(u);
-        entityManager.persist(v3);
-
-        Vehicle v4 = new Vehicle();
-        //v4.setId(4);
-        v4.setEnabled(true);
-        v4.setPlate("PAULA");
-        v4.setBrand("Toyota");
-        v4.setModel("Corolla");
-        v4.setSize("M");
-        v4.setParker(u1);
-        entityManager.persist(v4);
-
-        Vehicle v5 = new Vehicle();
-        //v5.setId(5);
-        v5.setEnabled(true);
-        v5.setPlate("ARTURO");
-        v5.setBrand("Honda");
-        v5.setModel("Civic");
-        v5.setSize("L");
-        v5.setParker(u1);
-        entityManager.persist(v5);
-
-        Vehicle v6 = new Vehicle();
-        //v6.setId(6);
-        v6.setEnabled(true);
-        v6.setPlate("DEF9876");
-        v6.setBrand("BMW");
-        v6.setModel("X5");
-        v6.setSize("XL");
-        v6.setParker(u1);
-        entityManager.persist(v6);
-
-        Reserve r1 = new Reserve();
-        //r1.setId(1);
-        r1.setState(Reserve.State.CONFIRMED);
-        r1.setStartDate(LocalDate.parse("2025-03-06"));
-        r1.setEndDate(LocalDate.parse("2025-03-07"));
-        r1.setStartTime(LocalTime.parse("10:00"));
-        r1.setEndTime(LocalTime.parse("12:00"));
-        r1.setPrice(5.0);
-        r1.setComments("Reserva para evento");
-        r1.setSpot(entityManager.find(Spot.class, 1L));
-        r1.setVehicle(v1);
-        entityManager.persist(r1);
-
-        Reserve r2 = new Reserve();
-        //r2.setId(2);
-        r2.setState(Reserve.State.CONFIRMED);
-        r2.setStartDate(LocalDate.parse("2025-03-06"));
-        r2.setEndDate(LocalDate.parse("2025-03-07"));
-        r2.setStartTime(LocalTime.parse("10:00"));
-        r2.setEndTime(LocalTime.parse("12:00"));
-        r2.setPrice(8.0);
-        r2.setComments("Reserva para evento");
-        r2.setSpot(entityManager.find(Spot.class, 2L));
-        r2.setVehicle(v2);
-        entityManager.persist(r2);
-
-        Request req1 = new Request();
-        req1.setEnabled(true);
-        req1.setName("Interparkking el mercado");
-        req1.setAddress("Calle cerrajeros s/n");
-        req1.setCp(28801);
-        req1.setCity("Alcala de Henares");
-        req1.setTelephone(918798072);
-        req1.setEmail("interparking@gmail.com");
-        req1.setFeePerHour(1.1);
-        req1.setOpeningTime(LocalTime.parse("00:00"));
-        req1.setClosingTime(LocalTime.parse("00:00"));
-        req1.setLatitude(40.48205);
-        req1.setLongitude(-3.36553);
-        req1.setIdParking(-1);
-        //req1.setTotalSpots(20); // TODO
-        req1.setState("AÑADIR");
-        req1.setEnterprise(e);
-        entityManager.persist(req1);
-
-        /* TODO
-         * INSERT INTO REQUEST (id, enabled, name, address, cp, city, country, telephone, email, feePerHour, openingTime, closingTime, longitude, latitude, idParking, totalSpots, state, enterprise_id)
-VALUES (1, true, 'InterParking El Mercado', 'Calle Cerrajeros s/n', 28801, 'Alcalá de Henares', 'Espanya', '918798072', 'interparking@gmial.com', 1.1, '00:00', '00:00', 40.48205, -3.36553, null, 20, 'ANYADIR', 3);
-         */
 
         return "index";
     }
