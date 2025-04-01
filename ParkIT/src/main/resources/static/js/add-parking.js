08ae17699092eaaf9db1899044d3916049c0f139
Alexboni97 <alexboni@ucm.es>
2025-04-01 14:45:48 +0200

    request notification send

---
+++ ParkIT/src/main/resources/static/js/add-parking.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () { //Para que no lo cargue directamente
+    
+    let b= document.getElementById("enviar-solicitud");
+    b.onclick = (e) => {
+        e.preventDefault();
+        go("/enterprise/request-parking", 'POST', {
+            name: document.getElementById("name").value,
+            address: document.getElementById("address").value,
+            city: document.getElementById("city").value,
+            country: document.getElementById("country").value,
+            cp: document.getElementById("cp").value,
+            openingTime: document.getElementById("openingTime").value,
+            closingTime: document.getElementById("closingTime").value,
+            totalSpots: document.getElementById("totalSpots").value,
+            feePerHour: document.getElementById("feePerHour").value,
+            telephone: document.getElementById("telephone").value,
+            email: document.getElementById("email").value
+            
+        })
+        .then(d => {
+            console.log("happy", d)
+            window.location.href = "/enterprise/enterprise-requests";
+        })
+        .catch(e => console.log("sad", e))
+        
+    }
+});
